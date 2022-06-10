10515896e6fa4efff45539a167ca2c1ec8535de6
ortlaz <anastasea.com@gmail.com>
2022-06-10 21:12:47 +0300

    фикс БД

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'M00nL1ghT'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False #отключение оповещений об ошибках в БД
-	SQLALCHEMY_DATABASE_URI = 'postgresql://root:laibach@localhost:5432/ratings' #URL БД
+	SQLALCHEMY_DATABASE_URI = 'postgresql://root:laibach@localhost:5432/postgres' #URL БД
 	FOLDER_FOR_FILES = '/rate/app/uploads'
 	ALLOWED_FILES = set(['xlsx', 'xlsm'])
 	POSTS_ON_PAGE = 5
